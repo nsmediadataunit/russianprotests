4485bb4975ecf738a7f6124ca6002820a93ba074
Michael Goodier <michaelgoodier7@gmail.com>
2022-03-04 11:25:15 +0000

    update, add pop to city

--- R/scrape_detainees.r
+++ R/scrape_detainees.r
@@ -127,7 +127,7 @@ write_excel_csv(missing_city_locations,"data/missing_city_locations.csv")
 
 
 #join data
-data_geo <- data %>% left_join(locations) %>% left_join(city_locations)
+data_geo <- data %>% left_join(locations) %>% left_join(city_locations) 
 
 write_excel_csv(data_geo,paste0("data/detainees/latest_daily_detainees_district.csv"))
 
@@ -135,9 +135,10 @@ write_excel_csv(data_geo,paste0("data/detainees/latest_daily_detainees_district.
 cumulative_district <- data_geo %>% group_by(city,district,city_en,district_en) %>%
   summarise(cumulative_detainees = sum(district_detainees,na.rm=T))
 write_excel_csv(data,paste0("data/detainees/latest_cumulative_detainees_district.csv"))
-cumulative_city <- data_geo %>% select(city,city_en,city_detainees,city_lat,city_lon) %>% unique() %>% group_by(city,city_en,city_lat,city_lon)%>%
-  summarise(cumulative_detainees = sum(as.numeric(city_detainees),na.rm=T))
-city_dw <- cumulative_city %>% ungroup() %>%select(Lat=city_lat,Lon=city_lon,Title=city_en,cumulative_detainees)
+cumulative_city <- data_geo %>% left_join(read_csv("data/city_pop.csv")) %>% select(city,city_en,city_pop,city_detainees,city_lat,city_lon) %>% unique() %>% group_by(city,city_en,city_pop,city_lat,city_lon)%>%
+  summarise(cumulative_detainees = sum(as.numeric(city_detainees),na.rm=T)) %>%
+  mutate(cumulative_detainees_per_1000000 = 1000000*(cumulative_detainees/city_pop))
+city_dw <- cumulative_city %>% ungroup() %>%select(Lat=city_lat,Lon=city_lon,Title=city_en,cumulative_detainees,cumulative_detainees_per_1000000)
 write_excel_csv(city_dw,"data/city_dw.csv")
 write_excel_csv(cumulative_city,paste0("data/detainees/latest_cumulative_detainees_city.csv"))
 #timeseries <- data %>% group_by(date) %>% summarise(sum_district_detainees = sum(district_detainees))
